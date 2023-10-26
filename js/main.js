be80816ab63a597309fa7007fc1081c5eee38481
Noah Rarick <noah.rarick@gmail.com>
2023-10-26 12:34:25 -0700

    adding initial pictures

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
-// Get the element with the class "icon"
-let icon = document.getElementsByClassName("icon")[0];
 
-// Add an event listener for the 'click' event on the icon element
-icon.addEventListener('click', responsive_control);
+// Get the video
+var video = document.getElementById("myVideo");
 
-// Function to control the responsiveness of the navigation bar
-function responsive_control() {
-  // Get the element with the id "myTopnav"
-  let x = document.getElementById("myTopnav");
+// Get the button
+var btn = document.getElementById("myBtn");
 
-  // Check if the class name of the element is "topnav"
-  if (x.className === "topnav") {
-    // If it is, add the "responsive" class to the element
-    x.className += " responsive";
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
   } else {
-    // If it's not, remove the "responsive" class from the element
-    x.className = "topnav";
+    video.pause();
+    btn.innerHTML = "Play";
   }
 }
+
+$(document).ready(function(){
+    $('#image-carousel').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+    });
+  });
