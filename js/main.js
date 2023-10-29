1802fa9945711cfbeb8bbb1c534fc513ecd22172
Noah Rarick <noah.rarick@gmail.com>
2023-10-29 15:50:35 -0700

    final changes

--- js/main.js
+++ js/main.js
@@ -16,12 +16,48 @@ function myFunction() {
   }
 }
 
-$(document).ready(function(){
-    $('#image-carousel').slick({
-      dots: true,
-      infinite: true,
-      speed: 500,
-      fade: true,
-      cssEase: 'linear'
-    });
-  });
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function playPause() {
+    const video = document.getElementById('video');
+    const playBtn = document.getElementById('playBtn');
+    const pauseBtn = document.getElementById('pauseBtn');
+
+    if(video.paused) {
+        video.play();
+        playBtn.style.display = "none";
+        pauseBtn.style.display = "block";
+    } else {
+        video.pause();
+        playBtn.style.display = "block";
+        pauseBtn.style.display = "none";
+    }
+}
+
